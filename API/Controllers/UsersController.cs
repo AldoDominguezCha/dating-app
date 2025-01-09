65a9434238b6052763d74146af93803f75cc3f8b
Aldo Dominguez <aldodominguezchz@gmail.com>
2025-01-09 15:53:16 -0600

    create initial skeleton implementation of the API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(DataContext context) : ControllerBase()
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            List<AppUser> users = await context.Users.ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetUser(int id) {
+            
+            AppUser? user = await context.Users.FindAsync(id);
+
+            if (user == null) return NotFound();
+
+            return Ok(user);
+        }
+    }
+}
